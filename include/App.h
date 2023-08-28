4a71af5141908bbc60ff1a0f077ac103d5dcb2fe
Liu Jiawei <liujw9922@mails.jlu.edu.cn>
2023-08-28 16:10:58 +0800

    Shrimp

---
+++ include/App.h
@@ -0,0 +1,9 @@
+// App.h
+#pragma once
+class App {
+public:
+	virtual ~App() = default;
+	virtual void init();	//初始化工作
+	virtual void run();	//运行
+	virtual void term();	//清理工作
+};
---
+++ include/MenuFactory.h
@@ -0,0 +1,16 @@
+// MenuFactory.h
+#pragma once
+#include "MenuType.h"
+
+//²Ëµ¥¹¤³§
+class Menu;
+class MenuFactory {
+public:
+	virtual ~MenuFactory() = default;
+	virtual Menu* createMenu(MenuType menuType) = 0;
+};
+
+class MyMenuFactory :public MenuFactory {
+public:
+	virtual Menu* createMenu(MenuType menuType);
+};
